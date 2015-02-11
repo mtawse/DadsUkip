dc101963510f4094ef9f31694b72d951283afb33
Martin Tawse <martintawse@gmail.com>
2015-02-11 00:37:58 +0000

    add github to exclude list

--- DadsUkip.user.js
+++ DadsUkip.user.js
@@ -4,6 +4,7 @@
 // @version    0.1
 // @description  Policies for 1950s
 // @include     *
+// @exclude https://github.com/*
 // @require http://code.jquery.com/jquery-latest.js
 // @copyright  2015+ Martin Tawse
 // ==/UserScript==
