d0331c4bc1982e4b9c8c6bceaa40672ee2f3bd1e
Martin Tawse <martintawse@gmail.com>
2015-03-28 19:15:30 +0000

    Overhaul with added random racicism

--- DadsUkip.user.js
+++ DadsUkip.user.js
@@ -3,34 +3,56 @@
 // @namespace  http://martintawse.com
 // @version    0.1
 // @description  Policies for 1950s
-// @include     *
-// @exclude https://github.com/*
-// @require http://code.jquery.com/jquery-latest.js
+// @include     http://www.theguardian.com/*
+// @include     http://www.bbc.com/news/*
+// @include     http://www.telegraph.co.uk/*
+// @include     http://www.independent.co.uk/*
+// @include     http://www.scotsman.com/*
+// @include     http://www.heraldscotland.com/news/*
 // @copyright  2015+ Martin Tawse
 // ==/UserScript==
 
-$(document).ready(function () {
+var replacementsMappings = {
+    'ukip' : "Dad's Army",
+    'Nigel Farage' : 'Captain Mainwaring',
+    'Farage' : 'Mainwaring'
+};
 
-    var elems = ['h1', 'h2', 'h3', 'h4', 'a', 'p'];
-    var replacementsMappings = {
-        'ukip' : "Dad's Army",
-        'Nigel Farage' : 'Captain Mainwaring',
-        'Farage' : 'Mainwaring'
-    };
+var notRacistComments = ['Farage said'];
 
-    for (var i = 0; i < elems.length; i++) {
-        $(elems[i]).each(function () {
-            var content = $(this).html();
+var walkTheDOM = function walk(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node) {
+        walk(node, func);
+        node = node.nextSibling;
+    }
+}
+
+// currently we'll assume Farage is racist 50% of the time
+var isFarageRacist = function () {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+
+walkTheDOM(document.body, function(node) {
+        if (node.nodeType == 3) {
+            var textContent = node.textContent;  
+            for (var i = 0; i < notRacistComments.length; i++) {
+                if (isFarageRacist()) {
+                    var re = new RegExp('('+notRacistComments[i]+')', 'gi');
+                    textContent = textContent.replace(re, notRacistComments[i]+' he\'s not racist but');
+                }
+            }
             for (var key in replacementsMappings) {
                 if (replacementsMappings.hasOwnProperty(key)) {
                     var re = new RegExp('('+key+')', 'gi');
-                    content = content.replace(re, replacementsMappings[key]);
+                    textContent = textContent.replace(re, replacementsMappings[key]);
                 }
-            }
-            $(this).html(content);
-        });
+        } 
+        node.textContent = textContent;
     }
 });
-
-
-
